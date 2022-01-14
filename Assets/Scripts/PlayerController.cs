cebe71c2ed02c8dffb801b4f7e6a2fd757f42752
ArHiFranki <franki550@gmail.com>
2022-01-14 12:00:45 +0700

    Добавил возможность ускорения при нажатии кнопки W

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float torqueAmount = 1f;
+    [SerializeField] private float boostSpeed = 30f;
+    [SerializeField] private float baseSpeed = 20f;
 
     private new Rigidbody2D rigidbody2D;
+    private SurfaceEffector2D surfaceEffector2D;
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     private void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
@@ -23,6 +33,18 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             rigidbody2D.AddTorque(-torqueAmount);
+        }
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
